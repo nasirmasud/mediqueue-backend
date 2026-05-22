7819750b25f145dfdac7fc77869038fc6fb1bff9
nasirmasud <nasir.masud@ymail.com>
2026-05-22 11:06:54 +0600

    Establish connection with mongodb and define collections

--- Index.js
+++ Index.js
@@ -1,3 +1,15 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -7,6 +19,17 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+async function run() {
+  try {
+    const db = client.db("mediqueue");
+    const tutorCollection = db.collection("tutors");
+    const tutorBookingCollection = db.collection("tutorBookings");
+  } catch (error) {
+    console.error(error);
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server is running fine");
